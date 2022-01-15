87033e9c0d5570004cee08a2fc3d110ea91e90e6
lezvieprod <lezviekgd@gmail.com>
2022-01-15 17:42:02 +0200

    fix missing pattern for dynamic tag in app

--- app/blogengine/blog/urls.py
+++ app/blogengine/blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', posts_list, name='posts_list_url'),
     path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),
     path('tags/', tags_list, name='tags_list_url'),
+    path('tag/<str:slug>', TagDetail.as_view(), name='tag_detail_url'),
 ]
